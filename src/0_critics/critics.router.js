6d5784e01d7aec058243dfc4c4b760637e8e9021
Samuel Yoo <samueyoo@gmail.com>
2022-09-08 19:43:53 -0500

    Initial commit; created initial migrations

---
+++ src/0_critics/critics.router.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const controller = require('./critics.controller');
+const cors = require('cors');
+
+router.use(cors());
+
+router
+    .
+
+module.exports = router;
