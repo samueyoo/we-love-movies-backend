229a058e0270b594ac6afb0e2f5d6b442a6d9162
Samuel Yoo <samueyoo@gmail.com>
2022-09-08 20:05:57 -0500

    Migrations created

--- src/db/migrations/20220908194215_createReviewsTable.js
+++ src/db/migrations/20220908194215_createReviewsTable.js
@@ -1,6 +1,15 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('reviews', table => {
+    table.incrememnts('review_id');
+    table.text('content');
+    table.integer('score');
+    table
+        .foreign('critic_id')
+        .reference('critic_id')
+        .inTable('critics')
+        .onDelete('')
+  })
 };
 
 exports.down = function(knex) {
