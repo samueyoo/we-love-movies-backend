b981b6114c991c986d35495e60652922f2100107
Samuel Yoo <samueyoo@gmail.com>
2022-09-19 20:13:01 -0500

    removed no longer needed commented code

--- src/routes/reviews/reviews.service.js
+++ src/routes/reviews/reviews.service.js
@@ -2,38 +2,10 @@ const knex = require('../../db/connection');
 const mapProperties = require('../../utils/map-properties');
 
 const addCritic = mapProperties({
-    // critic_id: 'critic.critic_id',
     preferred_name: 'critic.preferred_name',
     surname: 'critic.surname',
     organization_name: 'critic.organization_name',
-    // created_at: 'critic.created_at',
-    // updated_at: 'critic.updated_at'
 })
-
-// function read(reviewId) {
-//     return knex('reviews as r')
-//         .join('critics as c', 'r.critic_id', 'c.critic_id')
-//         .select('r.*', 'c.*')
-//         .where({ review_id: reviewId })
-//         .first()
-//         .then(data => data.map(addCritic))
-// }
-
-// function update(updatedReview, reviewId) {
-//     console.log(updatedReview)
-//     return knex('reviews')
-//         .select()
-//         .where({ review_id: reviewId })
-//         .update(updatedReview, '*');
-// } 
-
-
-// 
-// const addCritic = mapProperties({
-//     preferred_name: "critic.preferred_name",
-//     surname: "critic.surname",
-//     organization_name: "critic.organization_name"
-// });
 
 function read(reviewId) {
     return knex("reviews").select("*").where({ review_id: reviewId }).first();
